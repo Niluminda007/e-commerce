2db4a52da35483c7342c20e7cf24dd2374c3e7e9
Ushan Niluminda Jayawardana <princeofpersiatwathrones@gmail.com>
2023-08-23 16:42:28 +0300

    made changes to the form

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,6 +5,7 @@ import { UserLogin } from "../../Types/InterfaceTypes";
 import { UserAuth } from "../../context/AuthContext";
 import LoginAlternatives from "./LoginAlternatives";
 import Spinner from "../Spinner/Spinner";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const LoginForm = () => {
   const { handleSubmit } = useFormContext<UserLogin>();
@@ -13,13 +14,17 @@ const LoginForm = () => {
   const OnValid = async (data: UserLogin) => {
     await logIn(data);
   };
+  const { isMobile } = useMediaQuery();
 
   return (
-    <div className="flex flex-col gap-2 ">
+    <div
+      className={` ${
+        isMobile ? "h-screen w-screen items-center justify-center" : ""
+      } flex flex-col gap-2 `}>
       <form
         autoComplete="off"
         onSubmit={handleSubmit(OnValid)}
-        className="absolute sm:static z-10  left-[50%] -translate-x-[50%] translate-y-[40%] sm:translate-x-0 sm:translate-y-0 bg-cold_white sm:bg-transparent  flex flex-col items-center px-4 py-4 rounded-sm sm:rounded-none sm:px-6 sm:py-8">
+        className="sm:static z-10   bg-cold_white sm:bg-transparent  flex flex-col items-center px-4 py-4 rounded-sm sm:rounded-none sm:px-6 sm:py-8">
         <h1 className="text-dark_black text-[3rem] mb-8">Login</h1>
         <InputField name="userName" type="text" />
         <InputField name="password" type="password" />
